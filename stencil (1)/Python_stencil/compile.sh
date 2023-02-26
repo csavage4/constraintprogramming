2bfd5f481bffba7bab223895f8b63f73eb3de849
Colin Savage <77815412+csavage4@users.noreply.github.com>
2023-02-25 19:46:45 -0500

    Add files via upload

---
+++ stencil (1)/Python_stencil/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+
+# Update this file with instructions on how to compile your code
+pip3 install -r requirements.txt
---
+++ stencil (1)/Python_stencil/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+E_BADARGS=65
+if [ $# -ne 1 ]
+then
+	echo "Usage: `basename $0` <input>"
+	exit $E_BADARGS
+fi
+	
+input=$1
+
+# change this to point to your local installation
+# CHANGE it back to this value before submitting
+export CP_SOLVER_EXEC=/local/projects/cplex/CPLEX_Studio221/cpoptimizer/bin/x86-64_linux/cpoptimizer
+# run the solver
+python3.9 src/main.py $input
